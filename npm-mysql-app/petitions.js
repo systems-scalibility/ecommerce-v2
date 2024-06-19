1c1ca525dbaae386209c91ea63b39b80992a7a3c
Santiago Huanacu Sanchez <santiago.sanchez.dev@gmail.com>
2024-06-19 09:39:54 -0400

    [@santihsdev] Update logic for requests to the server

--- npm-mysql-app/petitions.js
+++ npm-mysql-app/petitions.js
@@ -13,15 +13,15 @@ const getAllByCodeNumber = async (codeNumber) => {
 
 const getAllByDate = async (startDate, endDate) => {
     const {data} = await request
-        .get(`api/SalesOrderItem/date?first=${startDate}&end=${endDate}`)
+        .get(`api/SalesOrderItem/range-dates?startDate=${startDate}&endDate=${endDate}`)
     console.log(data)
 }
 
-const getAllByQuantity = async (quantity) => {
-    const {data} = await request.get(`api/SalesOrderItem/quantity/${quantity}?condition='='`)
+const getAllByQuantity = async (quantity, condition) => {
+    const {data} = await request.get(`api/SalesOrderItem/quantity/${quantity}?condition=${condition}`)
     console.log(data)
 }
 
 await getAllByCodeNumber('P0002')
 await getAllByDate('2024-06-19 12:31:05', '2024-06-19 12:31:06')
-await getAllByQuantity(10)
+await getAllByQuantity(10, '=')
